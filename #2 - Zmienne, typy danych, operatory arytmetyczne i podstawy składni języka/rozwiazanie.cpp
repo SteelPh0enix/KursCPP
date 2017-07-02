344b134ddc823e0226886e458ba8a410642777a9
SteelPh0enix <phoenixpl512@gmail.com>
2017-07-02 15:46:00 +0200

    Added #2

---
+++ #2 - Zmienne, typy danych, operatory arytmetyczne i podstawy składni języka/rozwiazanie.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+	long long x, y;
+	std::cout << "Podaj liczby x i y: ";
+	std::cin >> x >> y;
+	
+	std::cout << "x + y = " << x + y << std::endl
+			  << "x - y = " << x - y << std::endl
+			  << "x * y = " << x * y << std::endl
+			  << "x / y = " << x / y << std::endl;
+	return 0;
+}
